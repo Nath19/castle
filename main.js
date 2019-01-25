2584550a6ef40a7dc4de48e7818ade2cf392996e
Unknown <nathanamar19@gmail.com>
2019-01-25 14:56:40 +0100

    get info of all hotels restaurant in  France

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const request = require("request");
 const fetch = require('node-fetch');
 const cheerio = require('cheerio');
-const url ='https://www.relaischateaux.com/us/destinations/europe';
+const castle = require("./castle.js").Castle;
+const url ='https://www.relaischateaux.com/us/destinations/france';
 
+/*
 function searchRestau(searchTerm)
 {
   return fetch(`${url}${searchTerm}`)
@@ -15,5 +17,11 @@ searchRestau('/france')
   .then(body => {
       console.log(body);
     });
+*/
 
-
+//get all hotel restaurants info
+    castle.getListHotels().then(response => {
+      const hotels = response;
+      console.log(hotels);
+    });
+    

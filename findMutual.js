e72633b759bfdb503f19d286beb7e1152467e504
Unknown <nathanamar19@gmail.com>
2019-02-05 22:34:21 +0100

    Add link for reservation

--- findMutual.js
+++ findMutual.js
@@ -7,7 +7,7 @@ var json3=[];
     for(var i=0;i<json1.length;i++){
         for(var j=0; j<json2.length; j++) {
             if(json1[i].name===json2[j].name) {
-                json3.push({"hotel Name ": json1[i].name,"hotel Price":json1[i].price,"Chef Name":json2[j].chef})
+                json3.push({"hotel Name ": json1[i].name,"hotel Price":json1[i].price,"Chef Name":json2[j].chef,"Link for Reservation":json1[i].link})
         }
 
     }

800c736bc6a6304caef83598d8440f57d9daaa45
Unknown <nathanamar19@gmail.com>
2019-02-05 22:16:46 +0100

    Add price hotel to  Json files

--- castle.js
+++ castle.js
@@ -28,10 +28,14 @@ var fichierJson = [];
             const link = await section.$$eval('.mainTitle3 > a ', as => as.map(a => a.href));
             const name = await section.$eval('h3 > a > span', span => span.innerText);
             const restaurant = await section.$eval('span', span => span.innerText);
+            var Price = -1;
+            if (await section.$('div > div:nth-child(2) > div.priceTag > div > span.price > span.price') !== null) {
+                Price = await section.$eval('div > div:nth-child(2) > div.priceTag > div > span.price > span.price', span => span.innerText);
+            }
             //We check if the hotels have Restaurants and we print the list in the console and in the JSON file
             if (restaurant === "Hotel + Restaurant") {
-                console.log("{\"name\":\"" + name + "\"," + "\"link\":\"" + link + "\"," + "\"genre\":\"" + restaurant + "\"}");
-                fichierJson.push({ "name": name, "link": link, "restaurant ": restaurant })
+                console.log("{\"name\":\"" + name + "\"," + "\"link\":\"" + link + "\"," + "\"genre\":\"" + restaurant + "\"," + "\"prix\":\"" + Price+ "\"}");
+                fichierJson.push({ "name": name, "link": link, "restaurant ": restaurant,"price":Price })
             }
         }
         //The URL's of all pages are weird, they are dispatched with the following order
@@ -52,9 +56,13 @@ var fichierJson = [];
                 const link = await section.$$eval('.mainTitle3 > a ', as => as.map(a => a.href));
                 const name = await section.$eval('h3 > a > span', span => span.innerText);
                 const restaurant = await section.$eval('span', span => span.innerText);
+                var Price = -1;
+                if (await section.$('div > div:nth-child(2) > div.priceTag > div > span.price > span.price') !== null) {
+                    Price = await section.$eval('div > div:nth-child(2) > div.priceTag > div > span.price > span.price', span => span.innerText);
+                }
                 if (restaurant === "Hotel + Restaurant") {
-                    console.log("{\"name\":\"" + name + "\"," + "\"link\":\"" + link + "\"," + "\"genre\":\"" + restaurant + "\"}");
-                    fichierJson.push({ "name": name, "link": link, "restaurant ": restaurant })
+                    console.log("{\"name\":\"" + name + "\"," + "\"link\":\"" + link + "\"," + "\"genre\":\"" + restaurant + "\"," + "\"prix\":\"" + Price+ "\"}");
+                    fichierJson.push({ "name": name, "link": link, "restaurant ": restaurant,"price":Price })
 
                 }
             }
--- findMutual.js
+++ findMutual.js
@@ -1,16 +1,16 @@
 var fs = require('fs');
 
 var json1 = require('./restauHotel.json');
-var json2 = require('./starredRestaurants.json'); 
+var json2 = require('./starredRestaurants.json');
 var json3=[];
-	// now compare their keys and values
-	for(var i=0;i<json1.length;i++){
-		for(var j=0; j<json2.length; j++) {
-			if(json1[i].name ===json2[j].name) {
-				json3.push({"hotel Name ": json1[i].name,"Chef Name":json2[j].chef})
-		}
+    // now compare their keys and values
+    for(var i=0;i<json1.length;i++){
+        for(var j=0; j<json2.length; j++) {
+            if(json1[i].name===json2[j].name) {
+                json3.push({"hotel Name ": json1[i].name,"hotel Price":json1[i].price,"Chef Name":json2[j].chef})
+        }
 
-	}
+    }
 }
 
 fs.writeFileSync("combinedJson.json",JSON.stringify(json3));
